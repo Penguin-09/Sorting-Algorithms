f908b28fbe425097d47ce4d2cfc2a096177b0c4f
Penguin <son@vdburg.site>
2025-05-02 13:20:01 +0200

    minor improvements

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 import { selectionSort } from "./selectionSort.js";
 
+const dataSizeInput = document.getElementById("dataSize");
+let stopSorting = false;
+
 /**
  * Fill and shuffle an array of data
  * @param {int} dataSize Size of the array to be filled and shuffled
@@ -47,101 +50,196 @@ function drawGraph(data) {
 
         canvas.appendChild(column);
     }
+
+    console.debug("Graph drawn");
 }
 
 /**
- * Print a report of the current graph on the current report element except for time calculation
+ * Print either the current or previous report
+ * @param {string} reportType Dictates the report type. Either "current" or "previous"
+ * @param {*} report report data, only needed for previous report
  */
-function printCurrentReport() {
-    // Algorithm type
-    const algorithmElement = document.getElementById("algorithmReportCurrent");
-    algorithmElement.innerHTML = document.getElementById("algorithmType").value;
-
-    // Data size
-    const dataSizeElement = document.getElementById("dataSizeReportCurrent");
-    dataSizeElement.innerHTML = dataSizeInput.value;
-
-    // Delay
-    const delayElement = document.getElementById("delayReportCurrent");
-    delayElement.innerHTML = document.getElementById("delay").value;
-}
-
-/**
- * Print a report of the previous graph on the previous report element
- */
-function printPreviousReport() {
-    if (localStorage.getItem("report")) {
-        const previousReport = JSON.parse(localStorage.getItem("report"));
+function printReport(reportType = "current") {
+    let previousReport = null;
+    if (reportType === "previous") {
+        previousReport = JSON.parse(localStorage.getItem("report"));
         console.debug("Previous report found: ", previousReport);
-
-        // Algorithm type
-        const algorithmElement = document.getElementById(
-            "algorithmReportPrevious"
-        );
-        algorithmElement.innerHTML = previousReport[0];
-
-        // Data size
-        const dataSizeElement = document.getElementById(
-            "dataSizeReportPrevious"
-        );
-        dataSizeElement.innerHTML = previousReport[1];
-        const delayElement = document.getElementById("delayReportPrevious");
-        delayElement.innerHTML = previousReport[2];
-
-        // Update report
-        const timeElement = document.getElementById("timeReportPrevious");
-        timeElement.innerHTML = `${previousReport[3]}s`;
     }
+
+    // Get elements to fill with report data
+    const algorithmElement =
+        reportType === "current"
+            ? document.getElementById("algorithmReportCurrent")
+            : document.getElementById("algorithmReportPrevious");
+    const calculationsElement =
+        reportType === "current"
+            ? document.getElementById("calculationsReportCurrent")
+            : document.getElementById("calculationsReportPrevious");
+    const delayElement =
+        reportType === "current"
+            ? document.getElementById("delayReportCurrent")
+            : document.getElementById("delayReportPrevious");
+
+    // Fill elements with report data
+    algorithmElement.innerHTML =
+        reportType === "current"
+            ? document.getElementById("algorithmType").value
+            : previousReport[0];
+    calculationsElement.innerHTML =
+        reportType === "current"
+            ? `0 / ${data.length}`
+            : `${previousReport[1]} / ${previousReport[2]}`;
+    delayElement.innerHTML =
+        reportType === "current"
+            ? `${document.getElementById("delay").value}ms`
+            : `${previousReport[3]}ms`;
+
+    console.debug("Report printed: ", reportType);
 }
 
-const dataSizeInput = document.getElementById("dataSize");
-
-/* Draw graph on page load */
-let data = fillAndShuffleData(dataSizeInput.value);
-drawGraph(data);
-
-/* Print previous report if needed */
-printPreviousReport();
-
-/* Data size input element */
-dataSizeInput.addEventListener("input", (event) => {
+/**
+ * Redraw the graph when the data size input changes
+ * @param {*} event The event object that is used to prevent default behavior
+ */
+function dataSizeInputHandler(event) {
     const newValue = event.target.value;
     console.debug("Data size changed: ", newValue);
     data = fillAndShuffleData(newValue);
     drawGraph(data);
-});
+    changeButtonState("submit", "enable");
+}
 
-/* Sort button */
-const submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click", async (event) => {
+/**
+ * Handler to stop the sorting process
+ * @param {*} event The event object that is used to prevent default behavior
+ */
+function stopButtonHandler(event) {
     event.preventDefault();
+    console.debug("Stop button clicked");
+    stopSorting = true;
+}
 
-    /* Print reports */
-    printPreviousReport();
-    printCurrentReport();
+/**
+ * Handler to reshuffle the data
+ * @param {*} event The event object that is used to prevent default behavior
+ */
+function reshuffleButtonHandler(event) {
+    event.preventDefault();
+    console.debug("Reshuffle button clicked");
+    data = fillAndShuffleData(dataSizeInput.value);
+    drawGraph(data);
+    changeButtonState("submit", "enable");
+}
+
+/**
+ * Enable or disable the specified button
+ * @param {*} buttonType Either "submit" or "stop"
+ * @param {*} state Either "enable" or "disable"
+ */
+function changeButtonState(buttonType = "submit", state = "enable") {
+    const button = document.getElementById(buttonType + "Button");
+
+    if (state === "enable") {
+        button.disabled = false;
+        button.classList.remove("opacity-50", "cursor-not-allowed");
+        button.classList.add("customHover");
+    } else if (state === "disable") {
+        button.disabled = true;
+        button.classList.add("opacity-50", "cursor-not-allowed");
+        button.classList.remove("customHover");
+    }
+
+    if (buttonType === "submit") {
+        submitButton.value = state === "enable" ? "Sort!" : "Sorting...";
+    }
+
+    console.debug(`${buttonType} button ${state}d`);
+}
+
+/**
+ * Enable or disable the form elements
+ * @param {string} state Either "enable" or "disable"
+ */
+function changeFormState(state = "enable") {
+    document.getElementById("dataSize").disabled =
+        state === "disable" ? true : false;
+    document.getElementById("delay").disabled =
+        state === "disable" ? true : false;
+    document.getElementById("algorithmType").disabled =
+        state === "disable" ? true : false;
+
+    console.debug(`Form ${state}d`);
+}
+
+/**
+ * The handler for the submit button. Starts the sorting proccess
+ * @param {*} event The event object that is used to prevent default behavior
+ */
+async function submitButtonHandler(event) {
+    event.preventDefault();
+    console.debug("Sort button clicked");
+    stopSorting = false;
+
+    // Print reports
+    if (localStorage.getItem("report")) printReport("previous");
+    printReport("current");
+
+    // Change button states
+    changeButtonState("submit", "disable");
+    changeButtonState("stop", "enable");
+    changeButtonState("reshuffle", "disable");
 
     // Disable form elements
-    const submitButton = document.getElementById("submitButton");
-    submitButton.disabled = true;
-    submitButton.value = "Sorting...";
-    submitButton.classList.add("opacity-50", "cursor-not-allowed");
-    submitButton.classList.remove("customHover");
+    changeFormState("disable");
 
-    document.getElementById("dataSize").disabled = true;
-    document.getElementById("delay").disabled = true;
-    document.getElementById("algorithmType").disabled = true;
+    // Start sorting
+    let report = null;
 
-    // Selection sort
-    let report = await selectionSort(
-        data,
-        document.getElementById("delay").value
-    );
+    switch (document.getElementById("algorithmType").value) {
+        case "selectionSort":
+            report = await selectionSort(
+                data,
+                document.getElementById("delay").value,
+                () => stopSorting
+            );
+            break;
+        default:
+            console.error("Invalid algorithm type selected");
+            break;
+    }
 
     console.debug("Sorting finished: ", report);
     localStorage.setItem("report", JSON.stringify(report));
 
+    // Change button states
+    changeButtonState("stop", "disable");
+    changeButtonState("reshuffle", "enable");
+    document.getElementById("submitButton").value = "Sorted!";
+
     // Enable form elements
-    document.getElementById("dataSize").disabled = false;
-    document.getElementById("delay").disabled = false;
-    document.getElementById("algorithmType").disabled = false;
-});
+    changeFormState("enable");
+}
+
+// Draw graph on page load
+let data = fillAndShuffleData(dataSizeInput.value);
+drawGraph(data);
+
+// Print previous report if needed
+if (localStorage.getItem("report")) printReport("previous");
+
+// Data size input element
+dataSizeInput.addEventListener("input", (event) => dataSizeInputHandler(event));
+
+// Stop button
+const stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", stopButtonHandler);
+
+// Reshuffle button
+const reshuffleButton = document.getElementById("reshuffleButton");
+reshuffleButton.addEventListener("click", (event) =>
+    reshuffleButtonHandler(event)
+);
+
+// Sort button
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", (event) => submitButtonHandler(event));
--- scripts/selectionSort.js
+++ scripts/selectionSort.js
@@ -2,15 +2,31 @@
  * Selection Sort sorts an array by taking the lowest number from the unsorted part and puts it at the end of the sorted part
  * @param {array} data Shuffled array of numbers
  * @param {number} delayMS Delay in milliseconds between each step of the algorithm
+ * @param {function} stopSorting Function to stop the sorting process
  * @returns {array} Array with report data
  */
-export async function selectionSort(data, delayMS = 100) {
+export async function selectionSort(
+    data,
+    delayMS = 100,
+    shouldStop = () => false
+) {
     console.debug("Selection sort started");
 
-    let timeElapsed = 0;
+    let calculations = 0;
 
     for (let i = 0; i < data.length; i++) {
         let smallestNumberIndex = i;
+
+        // Stop the algorithm if the stop button is pressed
+        if (shouldStop()) {
+            console.debug("Selection sort stopped");
+            return [
+                document.getElementById("algorithmType").value,
+                calculations,
+                document.getElementById("dataSize").value,
+                document.getElementById("delay").value,
+            ];
+        }
 
         // Find the smallest number
         for (let ii = i + 1; ii < data.length; ii++) {
@@ -19,21 +35,21 @@ export async function selectionSort(data, delayMS = 100) {
             }
         }
 
-        const smallestNumber = data[smallestNumberIndex];
+        let currentNumber = data[i];
 
         // Swap the smallest number with the first unsorted number
         if (smallestNumberIndex !== i) {
             // Store original values before swapping
-            const firstValue = data[i];
+            const firstValue = currentNumber;
             const smallestValue = data[smallestNumberIndex];
 
             // Swap the values in the data array
-            [data[i], data[smallestNumberIndex]] = [
+            [currentNumber, data[smallestNumberIndex]] = [
                 data[smallestNumberIndex],
-                data[i],
+                currentNumber,
             ];
 
-            // Update the DOM elements with their new heights
+            // Update the elements with their new heights
             const firstColumn = document.getElementById(`column-${firstValue}`);
             const smallestColumn = document.getElementById(
                 `column-${smallestValue}`
@@ -52,30 +68,27 @@ export async function selectionSort(data, delayMS = 100) {
             smallestColumn.id = `column-${firstValue}`;
         }
 
-        // Wait 100 ms
+        // Wait for a specified amount of time
         await new Promise((resolve) => {
             setTimeout(() => {
                 resolve();
             }, delayMS);
         });
 
-        timeElapsed += parseInt(delayMS);
+        calculations += 1;
 
         // Update report
-        const timeElement = document.getElementById("timeReportCurrent");
-        timeElement.innerHTML = `${timeElapsed / 1000}s`;
+        const calculationsElement = document.getElementById(
+            "calculationsReportCurrent"
+        );
+        calculationsElement.innerHTML = `${calculations} / ${data.length}`;
     }
 
-    // Enable sort button
-    submitButton.disabled = false;
-    submitButton.value = "Sort!";
-    submitButton.classList.remove("opacity-50", "cursor-not-allowed");
-    submitButton.classList.add("customHover");
-
+    // Return report data
     return [
         document.getElementById("algorithmType").value,
+        calculations,
         document.getElementById("dataSize").value,
         document.getElementById("delay").value,
-        timeElapsed / 1000,
     ];
 }
